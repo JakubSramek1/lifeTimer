d50e44cc78280de47aaa83ce1c304dec3eb6b4f8
JakubSramek1 <sramekvm@gmail.com>
2023-06-18 09:07:34 +0200

    definitions

---
+++ definitios/dates/dates.ts
@@ -0,0 +1,7 @@
+export const AverageLifeExpectancy = 77.28
+export const HoursPerDay = 24
+export const DaysPerWeek = 7
+export const DaysPerYear = 365.25
+export const SecondsPerHour = 3600
+export const SecondsPerMinute = 60
+export const MilisecondsPerSecond = 3600
